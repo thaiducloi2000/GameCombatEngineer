04624a749e1d34a5a223b0edf1364f3e80c13581
LITTLE-KID\ADMIN <thaiducloi2000@gmail.com>
2025-10-29 09:17:25 +0700

    Update Project

--- Source/GameCombatEngineer/GameCombatEngineer.Build.cs
+++ Source/GameCombatEngineer/GameCombatEngineer.Build.cs
@@ -4,10 +4,16 @@ using UnrealBuildTool;
 
 public class GameCombatEngineer : ModuleRules
 {
-	public GameCombatEngineer(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public GameCombatEngineer(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-	}
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+        PublicIncludePaths.AddRange(
+    new string[] { "GameCombatEngineer/Public" }
+);
+        PrivateIncludePaths.AddRange(
+            new string[] { "GameCombatEngineer/Private" }
+        );
+    }
 }
